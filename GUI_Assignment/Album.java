f4791bbe0ddaa1ce9d44e0cde57b9c3df2cd1a2b
JKhuu2 <67242928+JKhuu2@users.noreply.github.com>
2020-09-10 17:18:18 -0400

    Create Album.java

---
+++ GUI_Assignment/Album.java
@@ -0,0 +1,16 @@
+
+/**
+ * Homework 3 
+ * Jennifer Khuu, jtk2eh
+ * 
+ * Sources: Lecture Notes, Big Java book, TA Help
+ */
+
+public class Album extends PhotographContainer {
+
+    // constructor that calls the name in the super class photograph container
+    public Album(String name) {
+        super(name);
+
+    }
+}
